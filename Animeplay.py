a6a83584bac86f05df96ba82cfd2fa2501c597d8
Fernando Castillo <fer1234nando56@gmail.com>
2024-06-11 00:35:35 -0500

    Se hace una modificacion a play list de animes

---
+++ Animeplay.py
@@ -0,0 +1,15 @@
+print('Lista de anime favoritos')
+
+anime_lista = []
+
+while True:
+    añadir_anime = input('Introduce el nombre del anime: ')
+    anime_lista.append(añadir_anime)
+    continuar = input('¿Quieres añadir otro anime a la lista? (s/n): ')
+    if continuar.lower() != 's' and continuar.lower() != 'si' :
+        break
+
+# Convertir la lista a un string
+anime_texto = ', '.join(anime_lista)
+
+print(f'Tus animes agregados a la lista son : {anime_texto}')
